5dbd06424661d41c4156377fa584960df40ceef3
JYC11 <jaeyoon.c@gmail.com>
2023-03-16 18:18:28 +0900

    fix settings bug

--- src/app/db.py
+++ src/app/db.py
@@ -1,24 +1,8 @@
 from databases import Database
-from pydantic import BaseSettings, SecretStr
 from sqlalchemy import Column, DateTime, Integer, MetaData, String, Table, create_engine  # type: ignore
 from sqlalchemy.sql import func  # type: ignore
 
-
-class DBSettings(BaseSettings):
-    server: str = "localhost"
-    user: str = "jason"
-    password: SecretStr = SecretStr("")
-    db: str = "hello_fastapi_dev"
-    port: int = 5432
-    pool_size: int = 10
-    max_overflow: int = 10
-
-    @property
-    def url(self) -> str:
-        return "postgresql://{}:{}@{}:{}/{}".format(
-            self.user, self.password.get_secret_value(), self.server, self.port, self.db
-        )
-
+from .settings import DBSettings
 
 db_settings = DBSettings()
 
---
+++ src/app/settings.py
@@ -0,0 +1,21 @@
+from pydantic import BaseSettings, SecretStr
+
+
+class DBSettings(BaseSettings):
+    server: str = "localhost"
+    user: str = "jason"
+    password: SecretStr = SecretStr("")
+    db: str = "hello_fastapi_dev"
+    port: int = 5432
+    pool_size: int = 10
+    max_overflow: int = 10
+
+    @property
+    def url(self) -> str:
+        return "postgresql://{}:{}@{}:{}/{}".format(
+            self.user, self.password.get_secret_value(), self.server, self.port, self.db
+        )
+
+    class Config:
+        env_prefix = "postgres_"
+        env_file = ".env"
